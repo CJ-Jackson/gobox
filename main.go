1211bcebe59af6389611dbaabebb58ec9a3c2e8b
Christopher John Jackson <hello@cj-jackson.com>
2018-07-21 20:41:05 +0100

    Fix a small bug

--- main.go
+++ main.go
@@ -17,7 +17,7 @@ func main() {
 	env := tool.GetEnv()
 	file := env.ProjectBinPath() + "/" + strings.Trim(os.Args[1], "/")
 
-	cmd := exec.Command(tool.FixPath(file), os.Args[1:]...)
+	cmd := exec.Command(tool.FixPath(file), os.Args[2:]...)
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 	cmd.Stdin = os.Stdin
