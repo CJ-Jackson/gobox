02dd5a1cb69dc51aa1dc87fd70f3b72bddefe87d
Christopher John Jackson <hello@cj-jackson.com>
2018-07-23 10:11:36 +0100

    Use homedir instead of gopath

--- tool/env.go
+++ tool/env.go
@@ -10,10 +10,21 @@ import (
 	"os"
 	"os/exec"
 	"path"
+	"runtime"
 )
 
 type Env struct {
 	GoMod string `json:"GOMOD"`
+}
+
+func userHomeDir() string {
+	env := "HOME"
+	if runtime.GOOS == "windows" {
+		env = "USERPROFILE"
+	} else if runtime.GOOS == "plan9" {
+		env = "home"
+	}
+	return os.Getenv(env)
 }
 
 func (e Env) dirPath() string { return path.Dir(RevFixPath(e.GoMod)) }
@@ -23,10 +34,10 @@ func (e Env) ProjectBinPath() string { return e.dirPath() + "/bin" }
 func (e Env) ProjectConfigFile() string { return e.dirPath() + "/gobox.toml" }
 
 func (e Env) SandboxLocation() string {
-	goPath := os.Getenv("GOPATH")
+	homeDir := RevFixPath(userHomeDir())
 	hash := sha256.New()
 	hash.Write([]byte(e.GoMod))
-	return fmt.Sprintf("%s/.gobox/%s/sandbox", goPath, base64.URLEncoding.EncodeToString(hash.Sum(nil)))
+	return fmt.Sprintf("%s/.gobox/%s/sandbox", homeDir, base64.URLEncoding.EncodeToString(hash.Sum(nil)))
 }
 
 func GetEnv() Env {
