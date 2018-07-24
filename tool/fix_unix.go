35d6f6e823d48c49b659d15934cdb1c241dd0899
Christopher John Jackson <hello@cj-jackson.com>
2018-07-24 13:14:43 +0100

    Small clean up

--- tool/fix_unix.go
+++ tool/fix_unix.go
@@ -2,8 +2,6 @@
 
 package tool
 
-func FixPath(str string) string { return str }
-
-func RevFixPath(str string) string { return str }
-
+func FixPath(str string) string      { return str }
+func RevFixPath(str string) string   { return str }
 func FixOutput(output string) string { return output }
--- tool/fix_windows.go
+++ tool/fix_windows.go
@@ -6,8 +6,6 @@ import (
 	"strings"
 )
 
-func FixPath(str string) string { return strings.Replace(str, "/", "\\", -1) }
-
-func RevFixPath(str string) string { return strings.Replace(str, "\\", "/", -1) }
-
+func FixPath(str string) string      { return strings.Replace(str, "/", "\\", -1) }
+func RevFixPath(str string) string   { return strings.Replace(str, "\\", "/", -1) }
 func FixOutput(output string) string { return output + ".exe" }
